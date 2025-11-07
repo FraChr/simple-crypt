411c23578028194142498075eae8b2b06d4cfb64
FraChr <empynia@gmail.com>
2025-11-07 14:04:37 +0100

    updated main with try catch for unknown exceptions

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
-﻿#include "ArgsHandler.h"
+﻿#include <iostream>
+#include <ostream>
+#include "ArgsHandler.h"
 #include "commands/Commands.h"
 #include "FileHandling/FileHandler.h"
 
 int main(const int argc, char *argv[]) {
-    FileHandler fileHandler;
-    Commands cmd(fileHandler);
-    ArgsHandler argsHandler(cmd);
-    argsHandler.Handle(argc, argv);
+    try {
+        FileHandler fileHandler;
+        Commands cmd(fileHandler);
+        ArgsHandler argsHandler(cmd);
+        argsHandler.Handle(argc, argv);
+    } catch (const std::exception& e) {
+        std::cout << e.what() << std::endl;
+    }
     return 0;
 }
