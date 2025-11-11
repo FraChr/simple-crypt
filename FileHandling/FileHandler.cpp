d746336f246b08ac670cb587064813c9df1ddabc
FraChr <empynia@gmail.com>
2025-11-11 15:20:11 +0100

    moved progress to own private method

--- FileHandling/FileHandler.cpp
+++ FileHandling/FileHandler.cpp
@@ -1,6 +1,7 @@
 ﻿#include "FileHandler.h"
 #include <algorithm>
 #include <filesystem>
+#include <format>
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -10,38 +11,29 @@
 void FileHandler::writeToFile(const std::string &filename, const std::vector<unsigned char> &value) {
     std::ofstream file(filename, std::ofstream::binary);
     if(fileIsOpenCheck(file)) return;
-    /*if (!file.is_open()) {
-        RenderCmd::WriteError(FileError::FileNotOpen);
-        return;
-    }*/
 
     auto totalFileSize = static_cast<std::streamsize>(value.size());
     char fileChunk[4096];
     std::streamsize totalWritten = 0;
-    unsigned int percentWritten = 0;
     while (totalWritten < totalFileSize) {
-
         std::streamsize remaining = totalFileSize - totalWritten;
         std::streamsize chunkSize = std::min(remaining, static_cast<std::streamsize>(sizeof(fileChunk)));
         std::copy_n(value.begin() + totalWritten, chunkSize, fileChunk);
         file.write(fileChunk, chunkSize);
         totalWritten += chunkSize;
 
-        percentWritten = (totalWritten * 100) / totalFileSize;
-        std::string msg = "\rWriting: " + std::to_string(percentWritten) + '%';
-        RenderCmd::WriteOut(msg);
+        progressPercent(totalWritten, totalFileSize, "Writing");
     }
-
-
-    /*const std::vector <char> buffer(value.begin(), value.end());
-
-    file.write(buffer.data(), buffer.size());*/
 }
-/*template <typename T> void FileHandler::fileIsOpenCheck(const T &file) {
-    if (!file.is_open()) {
-        RenderCmd::WriteError(FileError::FileNotOpen);
-    }
-}*/
+
+void FileHandler::progressPercent(std::streamsize totalProcessed, std::streamsize totalFileSize, std::string prompt) {
+    unsigned int percentProcessed = (totalProcessed * 100 / totalFileSize);
+    /*std::string msg = "\r: " + std::to_string(percentProcessed) + '%';*/
+    std::string msg = std::format("\r{}: {} %", prompt, percentProcessed);
+    RenderCmd::WriteOut(msg);
+}
+
+
 template <typename T> bool FileHandler::fileIsOpenCheck(const T &file) {
     if (!file.is_open()) {
         RenderCmd::WriteError(FileError::FileNotOpen);
@@ -69,16 +61,11 @@ bool FileHandler::fileExists(const std::string& filename) {
 std::vector<unsigned char> FileHandler::readFromFile(const std::string& filename) {
     std::ifstream file(filename, std::ios::binary);
     if(fileIsOpenCheck(file)) return{};
-    /*if (!file.is_open()) {
-        RenderCmd::WriteError(FileError::FileNotOpen);
-        return {};
-    }*/
 
     auto start = std::chrono::high_resolution_clock::now();
 
     // Non buffered read time on 10gb .txt file 409.665 seconds
     // pre mem allocated vector with Buffered read time on 10gb .txt file 158.031 seconds
-    // Buffered read time on 10gb .txt file
     // Difference = 251.634 seconds
 
     file.seekg(0, std::ios::end);
@@ -86,41 +73,25 @@ std::vector<unsigned char> FileHandler::readFromFile(const std::string& filename
     file.seekg(0, std::ios::beg);
 
     std::vector<unsigned char> totalFile(totalFileSize);
-    /*std::vector<unsigned char> totalFile;*/
-
     char fileChunk[4096];
-
     std::streamsize totalRead = 0;
-    unsigned int percentRead = 0;
 
     while (totalRead < totalFileSize) {
         file.read(fileChunk, sizeof(fileChunk));
-
         std::streamsize currentRead = file.gcount();
-
-        /*std::copy(fileChunk, fileChunk + currentRead,totalFile.begin() + totalRead);*/
         std::copy_n(fileChunk, currentRead, totalFile.begin() + totalRead);
 
         totalRead += currentRead;
 
-        percentRead = (totalRead * 100) / totalFileSize;
-        std::string msg = "\rReading: " + std::to_string(percentRead) + '%';
-        RenderCmd::WriteOut(msg);
+        progressPercent(totalRead, totalFileSize, "Reading");
 
     }
 
-
-
-    /*std::vector<unsigned char> buffer{
-    std::istreambuf_iterator(file),
-    std::istreambuf_iterator<char>()
-    };*/
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
     std::cout << "\nelapsed time: " << elapsed_seconds.count() << " seconds\n";
     return totalFile;
 
-
-    /*return buffer;*/
-
 }
+
+
--- FileHandling/FileHandler.h
+++ FileHandling/FileHandler.h
@@ -1,7 +1,6 @@
 ﻿#pragma once
 #include <string>
 #include <vector>
-
 #include "../Interfaces/IFileHandler.h"
 
 class FileHandler : public IFileHandler {
@@ -9,7 +8,9 @@ class FileHandler : public IFileHandler {
         void writeToFile(const std::string& filename, const std::vector<unsigned char> &value) override;
         void writeToFile(const std::string& filename, const std::string &value) override;
         bool fileExists(const std::string& filename) override;
-        /*template <typename T> void fileIsOpenCheck(const T &file);*/
-        template <typename T> bool fileIsOpenCheck(const T &file);
         std::vector<unsigned char> readFromFile(const std::string& filename) override;
+    private:
+        template <typename T> bool fileIsOpenCheck(const T &file);
+        void progressPercent(std::streamsize totalProcessed, std::streamsize totalFileSize, std::string prompt);
+
 };
