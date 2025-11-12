428ef193a820ae6fe683f307a1ede694294d302e
FraChr <empynia@gmail.com>
2025-11-12 14:54:36 +0100

    added tests

---
+++ Tests/TestMocks.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+#include <string>
+
+#include "../Interfaces/IFileHandler.h"
+
+class MockFileHandler : public IFileHandler {
+public:
+    std::string writtenFile;
+    std::string writtenData;
+
+    void writeToFile(const std::string &file, const std::string &data) {
+        writtenFile = file;
+        writtenData = data;
+    }
+
+    void writeToFile(const std::string &filename, const std::vector<unsigned char> &value) override {
+        writtenFile = filename;
+        writtenData = std::string(value.begin(), value.end());
+    }
+    bool fileExists(const std::string &filename) override {
+        return true;
+    }
+    std::vector<unsigned char> readFromFile(const std::string &filename) override {
+        return{};
+    };
+};
---
+++ Tests/test.cpp
@@ -0,0 +1,49 @@
+﻿#include <fstream>
+#include <catch2/catch_test_macros.hpp>
+
+#include "TestMocks.h"
+#include "../ArgumentHandler/ArgsHandler.h"
+#include "../commands/Commands.h"
+#include "../FileHandling/FileHandler.h"
+#include "../Logger/Logger.h"
+#include "../Data/UI/UiText.h"
+#include "../Data/UI/ErrorText.h"
+
+TEST_CASE("File Handler", "[file]") {
+    FileHandler fileHandler;
+    SECTION("Read File") {
+        /*FileHandler fileHandler;*/
+
+        const auto data = fileHandler.readFromFile("testData.txt");
+
+        REQUIRE_FALSE(data.empty());
+    }
+    SECTION("Write File") {
+        /*FileHandler fileHandler;*/
+         fileHandler.writeToFile("TestingWriteMethod.txt", "Data being written by test");
+
+        CHECK_NOTHROW(fileHandler.writeToFile("Data being written by test",
+            "TestingWriteMethod.txt"));
+    }
+
+    SECTION("file exist") {
+        CHECK(fileHandler.fileExists("testData.txt"));
+        CHECK_FALSE(fileHandler.fileExists("ii.txt"));
+    }
+}
+
+
+TEST_CASE("Logger") {
+    MockFileHandler mockFileHandler;
+    Logger logger(mockFileHandler);
+
+    SECTION("GetCurrentFormatedTime()") {
+        logger.log(LogLevel::INFO, "test message");
+
+        REQUIRE(mockFileHandler.writtenFile == "LogData/Log.txt");
+        REQUIRE(mockFileHandler.writtenData.find("test message") != std::string::npos);
+        REQUIRE(mockFileHandler.writtenData.find("INFO") != std::string::npos);
+        REQUIRE(mockFileHandler.writtenData.front() == '[');
+
+    }
+}
