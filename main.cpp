6a8bc453712a9ce0093468216be6ff4935c47488
FraChr <empynia@gmail.com>
2025-11-12 15:04:43 +0100

    removed unused lines

--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
-﻿#include <assert.h>
-#include <iostream>
+﻿#include <iostream>
 #include <ostream>
 #include "ArgumentHandler/ArgsHandler.h"
 #include "commands/Commands.h"
 #include "FileHandling/FileHandler.h"
-
 #include <filesystem>
-
-#include "Data/UI/UiText.h"
 #include "Logger/Logger.h"
-#include "Render/RenderCmd.h"
 
 int main(const int argc, char *argv[]) {
     try {
@@ -18,11 +13,9 @@ int main(const int argc, char *argv[]) {
         Commands cmd(fileHandler, logger);
         ArgsHandler argsHandler(cmd);
         argsHandler.Handle(argc, argv);
-    } catch (const std::exception& e) {
+    } catch (const std::exception &e) {
         std::cout << e.what() << std::endl;
     }
 
-    /*FileHandler fileHandler;
-    Logger logger(fileHandler);
-    logger.log(Logger::ERROR, "SOMETHING WENT WRONG");*/
+
 }
